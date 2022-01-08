bd602c320e577063a190d8067dca3d65dc6d71ba
Rory James <84684031+rjames187@users.noreply.github.com>
2022-01-08 01:48:50 +0000

    Create navbar jsx skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home/Home.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
 function App() {
   return (
-    <div>
-      
+    <div id="parent-div">
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div>
+          Aeolus
+        </div>
+        <div>
+          <a href="">Home</a>
+          <a href="">Check Pollution</a>
+          <a href="">About Us</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
