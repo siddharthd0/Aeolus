d4f9b90c079396e8d113c52354464f474440af9e
RoryJames <84684031+rjames187@users.noreply.github.com>
2022-01-08 01:04:17 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      Aerolus
+      <p> test </p>
+      <h1> test </h1>
+    </main>
+  );
+}
+
+export default App;
